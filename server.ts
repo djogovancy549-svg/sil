90620664111b5801da7216bc35637e6171fd30f3
djogovancy549-svg <djogovancy549@gmail.com>
2026-09-25 19:21:32 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -33,7 +33,7 @@ async function ensureDataFiles() {
       drivePdfUrl: "",
       googleSheetUrl: "https://docs.google.com/spreadsheets/d/1CHwIRAvR7M3Sd3TdxgPYBYsdhUUqdGvFs_MzOVaMt3s/edit?usp=sharing",
       googleSheetEmbedUrl: "",
-      googleSheetAppsScriptUrl: "https://script.google.com/macros/s/AKfycbzZOivTRlr-cnrsLo18i_A8rKzINywqdVaGoTWp1AQwVyGxx2tLnulaHRw7mjIuT2Qw7Q/exec",
+      googleSheetAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxgYG8M_lTwtB1ucZ9sa-6Y5XUqmwPmTK1bT19rECQViQSf793fnfI390FwwcJpPK2_gQ/exec",
       adminPasscode: "publik2026"
     };
     await fs.writeFile(CONFIG_FILE, JSON.stringify(defaultConfig, null, 2), 'utf-8');
