74ee9c9c23178b1befc47374d6fd3cbcfd27b139
djogovancy549-svg <djogovancy549@gmail.com>
2026-09-25 18:16:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import bgWatermark from './assets/images/legal_green_watermark_1790310008588.jpg
 
 // Hardcoded Google Sheet & Webhook Apps Script URLs embedded directly into the coding
 const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1CHwIRAvR7M3Sd3TdxgPYBYsdhUUqdGvFs_MzOVaMt3s/edit?usp=sharing";
-const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzwpRA1kZDkW5GjcLCA8bCX6kjQxsN_kO5ideipNTKirtNkINisI2bXoeoyjjS299vrlQ/exec";
+const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyenIk6eCqHe8Q5lupOpFzMPBYYi0BuIctabL9Q034E1cAb70cuvJm3JeKfsLiRwRHwcg/exec";
 
 interface ActiveDraft {
   id: string;
